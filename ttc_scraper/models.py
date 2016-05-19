598b7a5a40e0e01a2ace809225e511e641a9c22e
Michael Bryan <michaelfbryan@gmail.com>
2016-05-19 09:18:23 +0800

    Created some models

---
+++ ttc_scraper/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import String, Column, Integer, ForeignKey
+
+
+
+Base = declarative_base()
+
+
+class Forum(Base):
+    __tablename__ = 'forums'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64), index=True, unique=True)
+
+    children = relationship('Thread')
+
+class Thread(Base):
+    __tablename__ = 'threads'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64), index=True, unique=True)
+
+    forum_id = Column(Integer, ForeignKey('forums.id'))
+    forums = relationship('Forum', backref='threads')
